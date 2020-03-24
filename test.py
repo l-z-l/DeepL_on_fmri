a64b5247717d09e00d962115959b5c50019806a1
wenzhao <z5155332@student.unsw.edu.au>
2020-03-24 16:59:36 +1100

    aseemble the preprocessor

--- test.py
+++ test.py
@@ -11,17 +11,33 @@ import numpy as np
 from glob import glob
 import matplotlib.pyplot as plt
 
+# specify the file directory
 data_path = "data/S150058"
-
 files = glob(data_path + '/*.dcm')
 
-
-slices = [dicom.read_file(s) for s in files]
+# read file and sort according to their instance number 
+slices = [ dicom.read_file(s) for s in files ]
 slices.sort(key = lambda x: float(x.InstanceNumber))
+indexes = [ x.InstanceNumber for x in slices ]
 
-index = [x.InstanceNumber for x in slices]
-position = [x.ImagePositionPatient[2] for x in slices]
 
+
+# convert to dictionary with key: instanceNumber, val: index 
+hash_map = {k: v for v, k in enumerate(indexes)}
+
+# show the position
+# position = [x.ImagePositionPatient[2] for x in slices]
+
+new_structure = []
+for i in range(1, 141): # instance number starts from 1
+    one_scan = []
+    for j in range(48):
+        if (i + j * 140) in hash_map.keys():
+            one_scan.append(hash_map[i + j * 140])
+            # one_scan.append(slices[hash_map[i + j * 140]])
+    new_structure.append(one_scan)
+            
+'''
 # 4976 5112
 
 sort_index = np.argsort(slices)
@@ -31,3 +47,4 @@ frames = np.ceil(len(slices) / 140)
 
 #for i in range(140):
  #   for j in range(frames):
+'''
